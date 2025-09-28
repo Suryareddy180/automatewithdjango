9606a69f9593f4672369f68a38cfffffe133cc7b
Surya Prakash <suryareddynallimilli@gmail.com>
2025-09-28 20:42:23 +0530

    commit

--- dataentry/management/commands/insertdata.py
+++ dataentry/management/commands/insertdata.py
@@ -14,12 +14,19 @@ class Command(BaseCommand):
         dataset=[
             {'roll_no':"22a81a6136",'name':'usha','age':20},
             {'roll_no':"22a81a6123",'name':'abhi','age':21},
-             {'roll_no':"22a81a6196",'name':'vamsi','age':22},
+            {'roll_no':"22a81a6196",'name':'vamsi','age':22},
+            {'roll_no':"22a81a6194",'name':'satya','age':22},
+             
              
         ]
         
         for data in dataset:
-            Student.objects.create(roll_no=data['roll_no'],name=data['name'],age=data['age'])
-            
-        self.stdout.write(self.style.SUCCESS('Data inserted sucessfully!'))
+            roll_no=data['roll_no']
+            existing_record=Student.objects.filter(roll_no=roll_no).exists()
+            if not existing_record:
+                Student.objects.create(roll_no=data['roll_no'],name=data['name'],age=data['age'])
+                self.stdout.write(self.style.SUCCESS('Data inserted sucessfully✅'))
+            else:
+                self.stderr.write(self.style.ERROR(f'{roll_no} is already exist!!!'))
+        
     
